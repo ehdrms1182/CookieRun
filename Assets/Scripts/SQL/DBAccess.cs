cd86f6b5b0c37a1158f7df1a12b915e55c0202b7
ehdrms1182 <ehdrms1182@gmail.com>
2021-11-28 14:45:21 +0900

    ControllerSetting

--- Assets/Scripts/SQL/DBAccess.cs
+++ Assets/Scripts/SQL/DBAccess.cs
@@ -12,7 +12,7 @@ public class DBAccess : MonoBehaviour
     }
     void DBCalling()
     {
-        Debug.Log("성공");
+        Debug.Log("DB연결");
         // conn = "URI=file:" + Application.dataPath + "/DB Browser로 만든 데이터베이스 이름.db";
         string conn = "URI=file:" + Application.dataPath + "/StreamingAssets/" + "/NetworkProgrammingB.db";
         // IDbConnection
@@ -31,8 +31,44 @@ public class DBAccess : MonoBehaviour
         while (reader.Read())
         {
             // 변수타입은 컬럼 데이터 타입에 맞추면 된다.
-            string UserID = reader.GetString(0);
-            string Password = reader.GetString(1);
+            string UserID = reader.GetString(0); //1번컬럼
+            string Password = reader.GetString(1); //2번컬럼
+            Debug.Log("연결 성공");
+            Debug.Log($"UserID = {UserID} + Password = {Password}");
+        }
+        // 닫아주고 초기화 시켜주는 곳
+        reader.Close();
+        reader = null;
+        dbcmd.Dispose();
+        dbcmd = null;
+        dbconn.Close();
+        dbconn = null;
+        Debug.Log("초기화 성공");
+    }
+    void DBWrite()
+    {
+        Debug.Log("DB작성");
+        // conn = "URI=file:" + Application.dataPath + "/DB Browser로 만든 데이터베이스 이름.db";
+        string conn = "URI=file:" + Application.dataPath + "/StreamingAssets/" + "/NetworkProgrammingB.db";
+        // IDbConnection
+        IDbConnection dbconn;
+        dbconn = (IDbConnection)new SqliteConnection(conn);
+        dbconn.Open(); //Open connection to the database.
+
+        // IDbCommand
+        IDbCommand dbcmd = dbconn.CreateCommand();
+        // sql문장 = "Insert 삽입할 테이블 컬럼 FROM 조회할 테이블";
+        string sqlQuery = $"Insert Into UserInfo(UserID, Password) VALUES(,)";
+        dbcmd.CommandText = sqlQuery;
+        
+
+        // IDataReader
+        IDataReader reader = dbcmd.ExecuteReader();
+        while (reader.Read())
+        {
+            // 변수타입은 컬럼 데이터 타입에 맞추면 된다.
+            string UserID = reader.GetString(0); //1번컬럼
+            string Password = reader.GetString(1); //2번컬럼
             Debug.Log("연결 성공");
             Debug.Log($"UserID = {UserID} + Password = {Password}");
         }
